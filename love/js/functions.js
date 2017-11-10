b14cb97aef99fa3b3f29e1a89e0623d7c0ad2a87
chenMhard <chenMhard@163.com>
2017-11-10 12:36:33 +0800

    love

--- love/js/functions.js
+++ love/js/functions.js
@@ -56,5 +56,6 @@ function timeElapse(date){
 		seconds = "0" + seconds;
 	}
 	var result = "第 <span class=\"digit\">" + days + "</span> 天 <span class=\"digit\">" + hours + "</span> 小时 <span class=\"digit\">" + minutes + "</span> 分钟 <span class=\"digit\">" + seconds + "</span> 秒"; 
+	var result = "第999999999999999999999999999天"; 
 	$("#clock").html(result);
 }
